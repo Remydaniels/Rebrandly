13bb2341a78afc3e1843f852c46c2180e04c5e7e
Remydaniels <danielremy01@gmail.com>
2023-11-26 14:26:53 -0600

    Accordion added

--- Clone Site/app.js
+++ Clone Site/app.js
@@ -4,3 +4,11 @@ const navItem = document.querySelector('.nav-items')
 hamburger.addEventListener('click', () => {
     navItem.classList.toggle('nav-item-open')
 })
+
+const accordion = document.getElementsByClassName ('AccordionItem__trigger')
+
+for (i = 0; i<accordion.length; i++ ){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
